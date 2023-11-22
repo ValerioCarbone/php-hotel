6c9895a7ad815701f530b86cf802ab66d3589dd0
ValerioCarbone <valeriocarbone10@gmail.com>
2023-11-22 17:15:31 +0100

    ADD - Bootstrap + Table

--- index.php
+++ index.php
@@ -46,22 +46,63 @@ $hotels = [
 // }
 ?>
 
-<?php foreach ($hotels as $hotel) {
-    foreach ($hotel as $hotelInfo => $value) {
-        if ($hotelInfo == 'parking' && $value === true) {
-?>
-            <p>
-                <?php echo 'La struttura è dotata di parcheggio'; ?>
-            </p>
-        <?php        } else if ($hotelInfo == 'parking' && $value === false) {
-        ?> <p>
-                <?php echo 'La struttura non è dotata di parcheggio'; ?>
-            </p>
-        <?php        } else {
-        ?> <p>
-                <?php echo $value; ?>
-            </p>
-<?php       }
-    }
-}
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotels</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col" class="text-center">Nome</th>
+                    <th scope="col" class="text-center">Descrizone</th>
+                    <th scope="col" class="text-center">Parking</th>
+                    <th scope="col" class="text-center">Voto</th>
+                    <th scope="col" class="text-center">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php foreach ($hotels as $hotel) {
+                ?> <tr> <?php foreach ($hotel as $hotelInfo => $value) {
+                            if ($hotelInfo == 'parking' && $value === true) { ?>
+                                <td class="text-center">
+                                    <?php echo 'La struttura è dotata di parcheggio'; ?>
+                                </td>
+                            <?php } else if ($hotelInfo == 'parking' && $value === false) {
+                            ?> <td class="text-center">
+                                    <?php echo 'La struttura non è dotata di parcheggio'; ?>
+                                </td>
+                            <?php } else if ($hotelInfo == 'distance_to_center') { ?>
+                                <td class="text-center">
+                                    <?php echo $value . ' Km'; ?>
+                                </td>
+                            <?php } else {
+                            ?> <td class="text-center">
+                                    <?php echo $value; ?>
+                                </td>
+                        <?php
+                            }
+                        } ?>
+                    </tr>
+                <?php
+                }
+                ?>
+
+            </tbody>
+        </table>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</html>
